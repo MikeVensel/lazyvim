96c8c66c9f078126f4ad70a517b97beb13bd28e7
Mike Vensel <michael.vensel@gmail.com>
2025-02-27 12:53:32 -0500

    initial commit

---
+++ lua/plugins/colorscheme.lua
@@ -0,0 +1,17 @@
+return {
+  -- add catpuccin
+  { "catppuccin/nvim" },
+  {
+    "LazyVim/LazyVim",
+    opts = {
+      colorscheme = "catppuccin",
+    },
+  },
+  -- { "rebelot/kanagawa.nvim" },
+  -- {
+  --   "LazyVim/LazyVim",
+  --   opts = {
+  --     colorscheme = "kanagawa",
+  --   },
+  -- },
+}
