14297b7b0e923e4dc4bdfaf29071481805e29408
Mike Vensel <mvensel@square-9.com>
2025-09-09 12:02:38 -0400

    fix(color scheme): add workaround for bufferline

--- lua/plugins/colorscheme.lua
+++ lua/plugins/colorscheme.lua
@@ -1,6 +1,15 @@
 return {
   -- add catpuccin
-  { "catppuccin/nvim" },
+  {
+    "catppuccin/nvim",
+    opts = function(_, opts)
+      local module = require("catppuccin.groups.integrations.bufferline")
+      if module then
+        module.get = module.get_theme
+      end
+      return opts
+    end,
+  },
   {
     "LazyVim/LazyVim",
     opts = {
